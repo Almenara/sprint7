edc229f21884ebdd4af89e89ef6cf8baf022deaf
albertoalmenara <PeopleRK>
2022-11-05 11:32:39 +0100

    Sprint 7 / Ejercicio 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
-import { PanelComponent } from './../panel/panel.component';
-
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder} from '@angular/forms';
 
 import { CartService } from './../services/cart.service';
 import { ProductsService } from './../services/products.service';
@@ -32,11 +30,22 @@ export class HomeComponent implements OnInit {
 
   sendToCart(productId:number, e:Event){
     let checked = (e.target as HTMLInputElement).checked;
-    let inputs = (e.target as HTMLInputElement).parentElement?.nextElementSibling?.querySelectorAll('input') ;
-    inputs?.forEach(input =>{
-      input.value = "0";
-    });
+    
     this.cartService.setCart({productId, featureId : null, quantity : Number(checked)});
+
+    if(checked){
+      let inputs = (e.target as HTMLInputElement).parentElement?.nextElementSibling?.querySelectorAll('input') ;
+      inputs?.forEach(input =>{
+        input.value = "1";
+      });
+      this.products.filter(product => { 
+        if(product.features.length)
+          product.features.map(feature => {
+            this.cartService.setFeaturesCart( product.id, {id: feature.id, quantity: 1})
+          })
+       })
+    }
+    
     this.setTotal();
     
   }
--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -2,7 +2,7 @@ import { feature } from './../product.interface';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CartService } from './../services/cart.service';
 import { product } from '../product.interface';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
 
 @Component({
@@ -21,18 +21,34 @@ export class PanelComponent implements OnInit {
 
   ngOnInit(): void {
     this.product.features.map(feature => {
-      this.panelForm.addControl('feature' + feature.id ,new FormControl(0, [Validators.required, Validators.min(1)]));
+      this.panelForm.addControl('feature' + feature.id ,new FormControl(1, [Validators.required, Validators.min(1)]));
+      this.cartService.setFeaturesCart( this.product.id, {id: feature.id, quantity: 1})
+      this.total()
     })
   }
   
   sendToCart(e: Event, featureId: number): void{
     this.cartService.setFeaturesCart( this.product.id, { id: featureId , quantity: Number((e.target as HTMLInputElement).value) });
   }
+
+  up(featureId: number){
+    let field = document.getElementById('feature' + featureId) as HTMLInputElement
+    let value:number = Number(field.value) +1;
+    field.value = String(value);
+    this.cartService.setFeaturesCart( this.product.id, {id: featureId, quantity: value})
+  }
+
+  down(featureId: number){
+    let field = document.getElementById('feature' + featureId) as HTMLInputElement
+    let value:number = Number(field.value) -1;
+    if(value > 0){
+      field.value = String(value);
+      this.cartService.setFeaturesCart( this.product.id, {id: featureId, quantity: value})
+    }    
+  }
   
   total(){
     this.addToTotal.emit();
   }
-  alerta(checked:boolean){
-    alert(checked)
-  }
+
 }
