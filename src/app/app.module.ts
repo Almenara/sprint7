fc79bd1276e6da4e665d51c867c3c3f896874492
albertoalmenara <PeopleRK>
2022-11-09 12:43:02 +0100

    Correcciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,7 +6,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { TotalService } from './services/total.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PanelComponent } from './panel/panel.component';
@@ -13,6 +13,10 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { ModalComponent } from './modal/modal.component';
 import { BudgetListComponent } from './budget-list/budget-list.component';
+import { TotalService } from './services/total.service';
+import { BudgetRouterService } from './services/budget-router.service';
+import { ProductsService } from './services/products.service';
+import { CartService } from './services/cart.service';
 
 const appRoutes:Routes = [
   {path: "", component:HomeComponent},
@@ -38,7 +42,10 @@ const appRoutes:Routes = [
     FormsModule
   ],
   providers: [
-    TotalService
+    TotalService,
+    CartService,
+    ProductsService,
+    BudgetRouterService
   ],
   bootstrap: [AppComponent]
 })
